ff611dc28ba4820bc70c28275fca76b604652642
digitalshade <digitalshadeuk@gmail.com>
2014-02-22 17:33:15 +0000

    Created skeleton ruby files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Library::Application.config.secret_key_base = '7332cd2045d216121572aa4fdf441003a1098b4e7726ff73cd6283ebcb568849842ac71e9189ddaa0b543acce9ac0b873a4430a463d2aa97c6a93be122054205'
